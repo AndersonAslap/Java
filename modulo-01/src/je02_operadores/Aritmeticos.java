292430982edc318e0d0fb6915161268677985bcb
Anderson Paiva <anderson.works1998@gmail.com>
2025-04-01 13:20:56 -0300

    feat: Aula de operadores aritmétricos e de atribuição

---
+++ modulo-01/src/je02_operadores/Aritmeticos.java
@@ -0,0 +1,20 @@
+package je02_operadores;
+
+public class Aritmeticos {
+    public static void main(String[] args) {
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+
+        int numero1 = 10;
+        int numero2 = 5;
+
+        // forma literal
+        numero2 = numero2 + numero1;
+
+        // forma abreviada
+        numero2 += numero1;
+    }
+}
---
+++ modulo-01/src/je02_operadores/Atribuicoes.java
@@ -0,0 +1,16 @@
+package je02_operadores;
+
+import java.util.Date;
+
+public class Atribuicoes {
+    public static void main(String[] args) {
+        String name = "ANDERSON";
+        String fullName = "Anderson" + " Paiva";
+        String fullName2 = "Anderson".concat(" Paiva");
+        int age = 26;
+        double heave = 90.0;
+        char gender = 'M';
+        boolean isTall = false;
+        Date birthDate = new Date();
+    }
+}
