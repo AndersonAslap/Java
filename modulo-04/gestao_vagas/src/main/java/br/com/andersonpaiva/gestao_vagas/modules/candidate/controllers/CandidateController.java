35185ff28ea39a727579445fb55c2d031dc3cd09
Anderson Paiva <anderson.works1998@gmail.com>
2025-04-26 21:52:49 -0300

    feat: Validando se um candidato já existe

--- modulo-04/gestao_vagas/src/main/java/br/com/andersonpaiva/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ modulo-04/gestao_vagas/src/main/java/br/com/andersonpaiva/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.andersonpaiva.gestao_vagas.modules.candidate.controllers;
 
+import br.com.andersonpaiva.gestao_vagas.exceptions.UserFoundException;
 import br.com.andersonpaiva.gestao_vagas.modules.candidate.models.CandidateEntity;
 import br.com.andersonpaiva.gestao_vagas.modules.candidate.repository.ICandidateRepository;
 import jakarta.validation.Valid;
@@ -18,6 +19,12 @@ public class CandidateController {
 
     @PostMapping
     public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        this.candidateRepository
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
         return this.candidateRepository.save(candidateEntity);
     }
 }
